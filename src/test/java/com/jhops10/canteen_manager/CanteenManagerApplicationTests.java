5ad67ffcded806e516c5eb14541327a0465ef86e
João Paulo <joaowais@gmail.com>
2025-08-17 12:56:13 -0300

    first commit

---
+++ src/test/java/com/jhops10/canteen_manager/CanteenManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhops10.canteen_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanteenManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
