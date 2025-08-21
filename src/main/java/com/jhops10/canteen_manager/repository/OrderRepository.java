88ba2ed7b091060ddc3ef64033d648d63a862ca8
João Paulo <joaowais@gmail.com>
2025-08-21 16:35:50 -0300

    feat(OrderRepository): adiciona método findByCustomerId

--- src/main/java/com/jhops10/canteen_manager/repository/OrderRepository.java
+++ src/main/java/com/jhops10/canteen_manager/repository/OrderRepository.java
@@ -3,5 +3,9 @@ package com.jhops10.canteen_manager.repository;
 import com.jhops10.canteen_manager.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByCustomerId(Long customerId);
 }
