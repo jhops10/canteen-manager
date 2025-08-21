38bae6e5998b90e3721f27e39d792544b00e2051
João Paulo <joaowais@gmail.com>
2025-08-21 16:34:50 -0300

    refactor(OrderResponseDTO): altera campos de OrderResponseDTO

--- src/main/java/com/jhops10/canteen_manager/dto/order/OrderResponseDTO.java
+++ src/main/java/com/jhops10/canteen_manager/dto/order/OrderResponseDTO.java
@@ -3,21 +3,24 @@ package com.jhops10.canteen_manager.dto.order;
 import com.jhops10.canteen_manager.dto.product.ProductResponseDTO;
 import com.jhops10.canteen_manager.model.Order;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record OrderResponseDTO(
         Long id,
-        Long customerId,
+        String customerName,
         ProductResponseDTO product,
         Integer quantity,
+        BigDecimal totalAmount,
         LocalDate purchaseDate
 ) {
     public static OrderResponseDTO fromEntity(Order entity) {
         return new OrderResponseDTO(
                 entity.getId(),
-                entity.getCustomer().getId(),
+                entity.getCustomer().getName(),
                 ProductResponseDTO.fromEntity(entity.getProduct()),
                 entity.getQuantity(),
+                entity.getProduct().getUnitValue().multiply(BigDecimal.valueOf(entity.getQuantity())),
                 entity.getPurchaseDate()
         );
     }
